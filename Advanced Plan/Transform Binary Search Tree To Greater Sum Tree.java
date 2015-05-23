ce04db138b95f22a72d75c0e50d15015601155f1
nanwan03 <wn1842@gmail.com>
2015-05-23 09:36:02 -0400

    commit

---
+++ Advanced Plan/Transform Binary Search Tree To Greater Sum Tree.java
@@ -0,0 +1,34 @@
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public TreeNode greaterSum(TreeNode root) {
+    // Write your solution here.
+    if (root == null) {
+      return root;
+    }
+    if (root.left == null && root.right == null) {
+      root.key = 0;
+      return root;
+    }
+    helper(root, 0);
+    return root;
+  }
+  private int helper(TreeNode root, int val) {
+    if (root == null) {
+      return val;
+    }
+    int rootVal = root.key;
+    int rightSum = helper(root.right, val);
+    root.key = rightSum;
+    int leftSum = helper(root.left, rightSum + rootVal);
+    return leftSum;
+  }
+}
