b5aee841697343f9f079433199b83c7c4771edf2
nanwan03 <wn1842@gmail.com>
2015-05-23 22:23:57 -0400

    commit

--- Advanced Plan/Max Sum Of Two Indices.java
+++ Advanced Plan/Max Sum Of Two Indices.java
@@ -4,29 +4,29 @@ public class Solution {
     if (array == null || array.length <= 1) {
       return new int[0];
     }
-    if (array.length == 2) {
-      return new int[]{0, 1};
-    }
-    int max = 0;
-    int maxIndex = 0;
-    for (int i = 0; i < array.length; ++i) {
-      if (array[i] > max) {
-        max = array[i];
-        maxIndex = i;
+    
+    int temp = 0;
+    int max1 = 0;
+    int maxIndex1 = 0;
+    int max2 = 0;
+    int maxIndex2 = 0;
+    
+    for (int i = 0; i < array.length; i++) {
+      temp = array[i] - i;
+      if (temp > max1) {
+        max1 = temp;
+        maxIndex1 = i;
+      }
+      temp = array[i] + i;
+      if (temp > max2) {
+        max2 = temp;
+        maxIndex2 = i;
       }
     }
-    int maxSum = 0;
-    int secondIndex = 0;
-    for (int i = 0; i < array.length; ++i) {
-      int temp = array[i] + max + (maxIndex >= i ? (maxIndex - i) : (i - maxIndex));
-      if (temp > maxSum) {
-        maxSum = temp;
-        secondIndex = i;
-      }
-    }
+    
     int[] rst = new int[2];
-    rst[0] = Math.min(secondIndex, maxIndex);
-    rst[1] = Math.max(secondIndex, maxIndex);
+    rst[0] = Math.min(maxIndex1, maxIndex2);
+    rst[1] = Math.max(maxIndex1, maxIndex2);
     return rst;
   }
 }
