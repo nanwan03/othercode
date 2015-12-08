50eaeb22fa156041deb92585bb0fa3a0c2a2673e
nanwan03 <wn1842@gmail.com>
2015-12-07 23:26:44 -0500

    commit

--- Classes/K Closest In Sorted Array.java
+++ Classes/K Closest In Sorted Array.java
@@ -1,34 +1,58 @@
 public class Solution {
-  private class Node {
-    int value;
-    int diff;
-    Node(int diff, int value) {
-      this.diff = diff;
-      this.value = value;
-    }
-    public int getValue() {
-      return value;
-    }
-  }
-  private class NodeComparator implements Comparator<Node> {
-    public int compare(Node a, Node b) {
-      return a.diff - b.diff;
-    }
-  }
   public int[] kClosest(int[] array, int target, int k) {
-    // Write your solution here
-    if(array == null || array.length == 0) {
-      return array;
-    }
-    List<Node> minusTarget = new ArrayList<Node>();
-    for (int i : array) {
-      minusTarget.add(new Node(Math.abs(i - target), i));
-    }
-    Collections.sort(minusTarget, new NodeComparator());
-    int[] rst = new int[k];
-    for (int i = 0; i < k; i++) {
-      rst[i] = minusTarget.get(i).getValue();
-    }
-    return rst;
+    if(array == null || array.length == 0 || k == 0) {
+          return new int[k];
+        }
+        int index = binarySearch(array, target);
+        int[] rst = new int[k];
+        int flag = 0;
+        if (index == 0) {
+        	for (flag = 0; flag < k; ++flag) {
+        		rst [flag] = array[flag];
+        	}
+        	return rst;
+        }
+        int left = index - 1;
+        int right = index;
+
+        while (left >= 0 && right < array.length) {
+        	if (Math.abs(array[left] - target) <= Math.abs(array[right] - target)) {
+        		rst[flag++] = array[left--];
+        	} else {
+        		rst[flag++] = array[right++];
+        	}
+        	if (flag == k) {
+        		return rst;
+        	}
+        }
+        
+        while (flag < k && left >= 0) {
+        	rst[flag++] = array[left--];
+        }
+        while (flag < k && right < array.length) {
+        	rst[flag++] = array[right++];
+        }
+        return rst;
   }
+  private int binarySearch(int[] A, int target) {
+    	int left = 0;
+        int right = A.length - 1;
+        while (left + 1 < right) {
+        	int mid = left + (right - left) / 2;
+        	if (A[mid] == target) {
+        		return mid;
+        	} else if (A[mid] < target) {
+        		left = mid + 1;
+        	} else {
+        		right = mid;
+        	}
+        }
+        if (A[left] == target) {
+        	return left;
+        } else if (A[right] == target) {
+        	return right;
+        } else {
+        	return A[left] > target ? left : right;
+        }
+    }
 }
