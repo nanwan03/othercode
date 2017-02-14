78589ef4bc50ea713c640b0bfd4db2675cc8129c
nanwan03 <wn1842@gmail.com>
2017-02-14 15:03:17 -0500

    commit

--- Classes/Fibonacci Number.java
+++ Classes/Fibonacci Number.java
@@ -1,21 +1,18 @@
 public class Solution {
   public long fibonacci(int K) {
-    // Write your solution here.
-    if (K <= 0) {
-      return 0;
+    // Write your solution here
+    if (K <= 1) {
+      return Math.max(K, 0);
     }
-    if (K <= 2) {
-      return 1;
-    }
-    long temp1 = 1;
-    long temp2 = 1;
-    for (int i = 3; i <= K; i++) {
-      if ((i & 0X01) == 1) {
-        temp1 += temp2;
+    long k1 = 0L;
+    long k2 = 1L;
+    for (int i = 2; i <= K; ++i) {
+      if ((i & 0x01) == 0) {
+        k1 += k2;
       } else {
-        temp2 += temp1;
+        k2 += k1;
       }
     }
-    return (K & 0X01) == 0 ? temp2 : temp1;
+    return (K & 0x01) == 0? k1 : k2;
   }
 }
