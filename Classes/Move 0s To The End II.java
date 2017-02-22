45fdb70266d9dce59c1a5fa4318929386d95972a
nanwan03 <wn1842@gmail.com>
2017-02-22 14:42:49 -0500

    commit

--- Classes/Move 0s To The End II.java
+++ Classes/Move 0s To The End II.java
@@ -1,21 +1,21 @@
 public class Solution {
   public int[] moveZero(int[] array) {
     // Write your solution here.
-    if (array == null || array.length == 0) {
+    if (array == null ||  array.length == 0) {
       return array;
     }
     int left = 0;
     int cur = 0;
     int right = array.length - 1;
     while (cur <= right) {
-      if (array[cur] == 0) {
-        swap(array, cur, right--);
+      if (array[cur] != 0) {
+        swap(array, left++, cur++);
       } else {
         cur++;
       }
     }
     return array;
-  }    
+  }
   private void swap(int[] array, int i, int j) {
     int temp = array[i];
     array[i] = array[j];
