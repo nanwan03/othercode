ac744b8f2c58f4a7ae95903352389021902ff512
nanwan03 <wn1842@gmail.com>
2017-02-25 22:02:20 -0500

    commit

--- Classes/Common Numbers Of Two Arrays I.java
+++ Classes/Common Numbers Of Two Arrays I.java
@@ -1,15 +1,18 @@
 public class Solution {
-  public List<Integer> common(int[] a, int[] b) {
+  public List<Integer> common(List<Integer> a, List<Integer> b) {
     // write your solution here
     List<Integer> rst = new ArrayList<Integer>();
-    if (a == null || a.length == 0 || b == null || b.length == 0) {
+    if (a == null || a.size() == 0 || b == null || b.size() == 0) {
       return rst;
     }
+    List<Integer> setList = a.size() > b.size() ? a : b;
+    List<Integer> traverseList = a.size() <= b.size() ? a : b;
+    Collections.sort(traverseList);
     Set<Integer> set = new HashSet<Integer>();
-    for (int i : a) {
+    for (int i : setList) {
       set.add(i);
     }
-    for (int i : b) {
+    for (int i : traverseList) {
       if (!set.add(i)) {
         rst.add(i);
       }
