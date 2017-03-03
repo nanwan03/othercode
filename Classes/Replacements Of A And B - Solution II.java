d08a7e94b58534b0cd5b2a1f61f94e37c0d9d895
nanwan03 <wn1842@gmail.com>
2017-03-03 17:07:07 -0500

    commit

--- Classes/Replacements Of A And B - Solution II.java
+++ Classes/Replacements Of A And B - Solution II.java
@@ -1,6 +1,9 @@
 public class Solution {
   public int minReplacements(String input) {
     // Write your solution here.
+    if (input == null || input.length() == 0) {
+      return 0;
+    }
   	int[] cache = new int[input.length()];
   	int count = 0;
   	for (int i = 0; i < input.length(); ++i) {
@@ -12,16 +15,13 @@ public class Solution {
     return helper(input, cache, input.length() - 1, 'b');
   }
   private int helper(String input, int[] cache, int index, char tail) {
-	  if (index < 0) {
-		  return 0;
-	  }
-	  if (tail == 'a') {
-		  return cache[index];
+	  if (index == 0) {
+	    return 0;
 	  }
 	  if (input.charAt(index) == 'a') {
-		  return Math.min(1 + helper(input, cache, index - 1, 'b'), helper(input, cache, index - 1, 'a'));
+		  return Math.min(1 + helper(input, cache, index - 1, 'b'), cache[index - 1]);
 	  } else {
-		  return Math.min(1 + helper(input, cache, index - 1, 'a'), helper(input, cache, index - 1, 'b'));
+		  return Math.min(1 + cache[index - 1], helper(input, cache, index - 1, 'b'));
 	  }
   }
 }
