a141291eb39d108c3a5646725d845bc12abb4e09
nanwan03 <wn1842@gmail.com>
2017-03-07 00:15:22 -0500

    commit

--- Classes/Form Largest Number.java
+++ Classes/Form Largest Number.java
@@ -1,50 +1,33 @@
 public class Solution {
+  class Cmp implements Comparator<String> {
+    @Override
+    public int compare(String a, String b) {
+      String ab = a.concat(b);
+      String ba = b.concat(a);
+      return ba.compareTo(ab);
+    }
+  }
   public String largestNumber(String[] input) {
     // Write your solution here.
-	if (input == null || input.length == 0) {
-		return null;
-	}
-	if (input.length == 1) {
-		return input[0];
-	}
-	String str = null;
-	return helper(str, input, 0);
-  }
-  private String helper(String str, String[] input, int pos) {
-	  if (pos == input.length) {
-		  str = compare(str, input);
-		  return str;
-	  }
-	  for (int i = pos; i < input.length; ++i) {
-		  swap(input, i, pos);
-		  str = helper(str, input, pos + 1);
-		  swap(input, i, pos);
-	  }
-	  return str;
-  }
-  private void swap(String[] input, int i, int j) {
-	  String str = input[i];
-	  input[i] = input[j];
-	  input[j] = str;
-  }
-  private String compare(String str, String[] input) {
-	  StringBuilder sb = new StringBuilder();
-	  for (String s : input) {
-		  sb.append(s);
-	  }
-	  String target = sb.toString();
-	  if (str == null) {
-		  return target;
-	  } else {
-		  for (int i = 0; i < target.length(); ++i) {
-			  if (str.charAt(i) < target.charAt(i)) {
-				  return target;
-			  } else if (str.charAt(i) > target.charAt(i)) {
-				  return str;
-			  }
-		  }
-	  }
-	  return str;
-	  
+    if (input == null || input.length < 1) {
+      return "";
+    }
+ 
+    Arrays.sort(input, new Cmp());
+    
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < input.length; ++i) {
+    	sb.append(input[i]);
+    }
+    
+    String rst = sb.toString();
+    int i = 0;
+    while (i < input.length && rst.charAt(i) == '0') {
+        i++;
+    }
+    if (i == input.length) {
+      return "0";
+    }
+    return rst.substring(i);
   }
 }
