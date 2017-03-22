1c90134adc329c7f3004fad959adf186aee765a6
nanwan03 <wn1842@gmail.com>
2017-03-22 15:47:58 -0400

    commit

--- Classes/Two Subsets With Min Difference.java
+++ Classes/Two Subsets With Min Difference.java
@@ -1,42 +1,28 @@
 public class Solution {
-	/*
-	   * Input has negative elements
-	   * Have limitation on the size of subset : n / 2;
-	   */
   public int minDifference(int[] array) {
     // Write your solution here.
-    List<List<Integer>> rst = new ArrayList<List<Integer>>();
-    if (array == null || array.length == 0) {
-        return 0;
-    }
-    Arrays.sort(array);
-    List<Integer> items = new ArrayList<Integer>();
-    helper(rst, items, array, 0);
     int sum = 0;
-    int min = Integer.MAX_VALUE;
     for (int i : array) {
       sum += i;
     }
-    for (List<Integer> list : rst) {
-      if (list.size() == array.length / 2) {
-        int temp = 0;
-        for (int i : list) {
-          temp += i;
-        }
-        min = Math.min(min, Math.abs(temp - (sum - temp)));
+    Arrays.sort(array);
+    return helper(array, sum, 0, 0, 0);
+  }
+  private int helper(int[] array, int sum, int partialSum, int index, int size) {
+    if (size == array.length / 2) {
+      return Math.abs(partialSum - (sum - partialSum));
+    }
+    int min = Integer.MAX_VALUE;
+    for (int i = index; i < array.length; ++i) {
+      if (i > index && array[i] == array[i - 1]) {
+        continue;
       }
+      partialSum += array[i];
+      size++;
+      min = Math.min(min, helper(array, sum, partialSum, i + 1, size));
+      size--;
+      partialSum -= array[i];
     }
     return min;
-  }
-  private void helper(List<List<Integer>> rst, List<Integer> items, int[] num, int pos) {
-      rst.add(new ArrayList<Integer>(items));
-      for (int i = pos; i < num.length; i++) {
-          if (i > pos && num[i] == num[i - 1]) {
-              continue;
-          }
-          items.add(num[i]);
-          helper(rst, items, num, i + 1);
-          items.remove(items.size() - 1);
-      }
   }
 }
