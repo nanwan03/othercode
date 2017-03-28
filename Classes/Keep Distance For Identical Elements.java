dcea3deb901aee12678d509df033561c3eacda83
nanwan03 <wn1842@gmail.com>
2017-03-27 23:14:55 -0400

    commit

--- Classes/Keep Distance For Identical Elements.java
+++ Classes/Keep Distance For Identical Elements.java
@@ -1,37 +1,34 @@
 public class Solution {
   public int[] keepDistance(int k) {
     // Write your solution here.
-    if (k <= 2) {
+    if (k == 0) {
       return null;
     }
-    int[] items = new int[k * 2];
     boolean[] isused = new boolean[k + 1];
-    return helper(items, 0, isused, k);
+    return helper(new int[k * 2], k, isused, 0);
   }
-  private int[] helper(int[] items, int index, boolean[] isused, int k) {
+  private int[] helper(int[] items, int k, boolean[] isused, int index) {
     if (index == items.length) {
-      return Arrays.copyOf(items, items.length);
+      return items;
     }
-    int[] rst = null;
     for (int i = 1; i <= k; ++i) {
-      if (!isused[i] && index + 1 + i < items.length
-        && items[index] == 0 && items[index + 1 + i] == 0) {
-          items[index] = i;
-          items[index + 1 + i] = i;
-          isused[i] = true;
-          int nextIndex = index + 1;
-          while (nextIndex < items.length && items[nextIndex] != 0) {
-            nextIndex++;
-          }
-          rst = helper(items, nextIndex, isused, k);
-          if (rst != null) {
-            return rst;
-          }
-          isused[i] = false;
-          items[index] = 0;
-          items[index + 1 + i] = 0;
+      if (!isused[i] && index + 1 + i < items.length 
+          && items[index] == 0 && items[index + 1 + i] == 0) {
+        items[index] = i;
+        items[index + 1 + i] = i;
+        isused[i] = true;
+        int nextIndex = index + 1;
+        while (nextIndex < items.length && items[nextIndex] != 0) {
+          nextIndex++;
         }
+        if (helper(items, k, isused, nextIndex) != null) {
+          return items;
+        }
+        isused[i] = false;
+        items[index] = 0;
+        items[index + 1 + i] = 0;
+      }
     }
-    return rst;
+    return null;
   }
 }
