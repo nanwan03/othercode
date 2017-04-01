f56dd4e726d41a35ad2d77463aa412810c75c400
nanwan03 <wn1842@gmail.com>
2017-04-01 16:37:44 -0400

    commit

--- Classes/Maximum Path Sum Binary Tree I.java
+++ Classes/Maximum Path Sum Binary Tree I.java
@@ -10,27 +10,38 @@
  */
 public class Solution {
   private class ResultType {
-	  int singlePath;
-	  int maxPath;
-	  ResultType(int singlePath, int maxPath) {
-		  this.singlePath = singlePath;
-		  this.maxPath = maxPath;
-	  }
-  }
-  private ResultType helper(TreeNode root) {
-	  if (root == null) {
-		  return new ResultType(Integer.MIN_VALUE, Integer.MIN_VALUE);
-	  }
-	  ResultType left = helper(root.left);
-	  ResultType right = helper(root.right);
-	  int singlePath = left.singlePath == Integer.MIN_VALUE && right.singlePath == Integer.MIN_VALUE ? root.key : Math.max(left.singlePath, right.singlePath) + root.key;
-	  int maxPath = Math.max(Math.max(left.maxPath, right.maxPath), left.singlePath == Integer.MIN_VALUE || right.singlePath == Integer.MIN_VALUE ? Integer.MIN_VALUE : left.singlePath + right.singlePath + root.key);
-	  return new ResultType(singlePath, maxPath);
+    int singlePath;
+    int maxPath;
+    ResultType(int singlePath, int maxPath) {
+      this.singlePath = singlePath;
+      this.maxPath = maxPath;
+    }
   }
   public int maxPathSum(TreeNode root) {
+    // Write your solution here.
     if (root == null) {
-    	return Integer.MIN_VALUE;
+      return Integer.MIN_VALUE;
     }
     return helper(root).maxPath;
+  }
+  private ResultType helper(TreeNode root) {
+    if (root == null) {
+      return new ResultType(Integer.MIN_VALUE, Integer.MIN_VALUE);
+    }
+    ResultType left = helper(root.left);
+    ResultType right = helper(root.right);
+    int singlePath;
+    if (root.left == null && root.right == null) {
+      singlePath = root.key;
+    } else {
+      singlePath = Math.max(left.singlePath, right.singlePath) + root.key;
+    }
+    int maxPath;
+    if (root.left == null || root.right == null) {
+      maxPath = Math.max(left.maxPath, right.maxPath);
+    } else {
+      maxPath = Math.max(Math.max(left.maxPath, right.maxPath), left.singlePath + right.singlePath + root.key);
+    }
+    return new ResultType(singlePath, maxPath);
   }
 }
