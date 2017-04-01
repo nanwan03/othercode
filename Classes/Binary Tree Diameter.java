2274e5e236cbbc94aaa342fff63dc9951f68d740
nanwan03 <wn1842@gmail.com>
2017-04-01 16:25:06 -0400

    commit

--- Classes/Binary Tree Diameter.java
+++ Classes/Binary Tree Diameter.java
@@ -1,3 +1,4 @@
+
 /**
  * public class TreeNode {
  *   public int key;
@@ -10,28 +11,33 @@
  */
 public class Solution {
   private class ResultType {
-	  int singlePath;
-	  int maxPath;
-	  ResultType(int singlePath, int maxPath) {
-		  this.singlePath = singlePath;
-		  this.maxPath = maxPath;
-	  }
-  }
-  private ResultType helper(TreeNode root) {
-	  if (root == null) {
-		  return new ResultType(0, 0);
-	  }
-	  ResultType left = helper(root.left);
-	  ResultType right = helper(root.right);
-	  int singlePath = Math.max(left.singlePath, right.singlePath) + 1;
-	  int maxPath = Math.max(Math.max(left.maxPath, right.maxPath), left.singlePath == 0 || right.singlePath == 0 ? Integer.MIN_VALUE : left.singlePath + right.singlePath + 1);
-	  return new ResultType(singlePath, maxPath);
+    int singlePath;
+    int maxPath;
+    ResultType(int singlePath, int maxPath) {
+      this.singlePath = singlePath;
+      this.maxPath = maxPath;
+    }
   }
   public int diameter(TreeNode root) {
+    // Write your solution here.
     if (root == null) {
-    	return 0;
+      return 0;
     }
-    
     return helper(root).maxPath;
+  }
+  private ResultType helper(TreeNode root) {
+    if (root == null) {
+      return new ResultType(0, 0);
+    }
+    ResultType left = helper(root.left);
+    ResultType right = helper(root.right);
+    int singlePath = Math.max(left.singlePath, right.singlePath) + 1;
+    int maxPath;
+    if (root.left == null || root.right == null) {
+      maxPath = Math.max(left.maxPath, right.maxPath);
+    } else {
+      maxPath = Math.max(Math.max(left.maxPath, right.maxPath),  left.singlePath + right.singlePath + 1);
+    }
+    return new ResultType(singlePath, maxPath);
   }
 }
