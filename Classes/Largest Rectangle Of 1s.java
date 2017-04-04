6e1df1bc4f8dfa29da3763c5e694c698e1fa937a
nanwan03 <wn1842@gmail.com>
2017-04-04 16:04:10 -0400

    commit

--- Classes/Largest Rectangle Of 1s.java
+++ Classes/Largest Rectangle Of 1s.java
@@ -1,39 +1,38 @@
+
 public class Solution {
   public int largest(int[][] matrix) {
+    // write your solution here
     if (matrix == null || matrix.length == 0) {
-    	return 0;
+      return 0;
     }
-    int[][] height = new int[matrix.length][matrix[0].length];
-    for (int i = 0; i < matrix.length; i++) {
-    	for (int j = 0; j < matrix[0].length; j++) {
-    	  if (matrix[i][j] == 0) {
-    	    height[i][j] = 0;
-    	  } else {
-    			height[i][j] = i == 0 ? 1 : height[i - 1][j] + 1;
-    		}
-    	}
-    }
+    int row = matrix.length;
+    int col = matrix[0].length;
+    int[] height = new int[col];
     int max = 0;
-    for (int i = 0; i < matrix.length; i++) {
-    	max = Math.max(max, getMax(height[i]));
+    for (int i = 0; i < row; ++i) {
+      for (int j = 0; j < col; ++j) {
+       if (matrix[i][j] == 0) {
+         height[j] = 0;
+       } else {
+         height[j] = 1 + height[j];
+       }
+      }
+      max = Math.max(max, getMax(height));
     }
     return max;
   }
   private int getMax(int[] height) {
-	  if (height == null || height.length == 0) {
-		  return 0;
-	  }
-	  int max = 0;
-	  Stack<Integer> stack = new Stack<Integer>();
-	  for (int i = 0; i <= height.length; i++) {
-		  int cur = i == height.length ? 0 : height[i];
-		  while (!stack.isEmpty() && cur <= height[stack.peek()]) {
-			  int h = height[stack.pop()];
-			  int w = stack.isEmpty() ? i : i - stack.peek() - 1;
-			  max = Math.max(max, h * w);
-		  }
-		  stack.push(i);
-	  }
-	  return max;
+    Stack<Integer> stack = new Stack<Integer>();
+    int max = 0;
+    for (int i = 0; i <= height.length; ++i) {
+      int cur = i == height.length ? 0 : height[i];
+      while (!stack.isEmpty() && cur <= height[stack.peek()]) {
+        int h  = height[stack.pop()];
+        int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+        max = Math.max(max, h * w);
+      }
+      stack.push(i);
+    }
+    return max;
   }
 }
