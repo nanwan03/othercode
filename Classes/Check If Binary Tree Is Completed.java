bb923ecf9598042586941c5b10cb3910d1a97627
nanwan03 <wn1842@gmail.com>
2017-04-05 14:42:15 -0400

    commit

--- Classes/Check If Binary Tree Is Completed.java
+++ Classes/Check If Binary Tree Is Completed.java
@@ -10,22 +10,30 @@
  */
 public class Solution {
   public boolean isCompleted(TreeNode root) {
-    // write your solution here
+    // Write your solution here.
     if (root == null) {
       return true;
     }
     Queue<TreeNode> queue = new LinkedList<TreeNode>();
     queue.offer(root);
-    boolean hasNull = false;
     while (!queue.isEmpty()) {
       TreeNode node = queue.poll();
       if (node == null) {
-        hasNull = true;
-      } else if (hasNull) {
-        return false;
+        return checkQueue(queue);
       } else {
         queue.offer(node.left);
         queue.offer(node.right);
+      }
+    }
+    return true;
+  }
+  private boolean checkQueue(Queue<TreeNode> queue) {
+    if (queue.isEmpty()) {
+      return true;
+    }
+    while (!queue.isEmpty()) {
+      if (queue.poll() != null) {
+        return false;
       }
     }
     return true;
