2ae4f50fa7b5b4f26d2168e549875199d2331ac7
nanwan03 <wn1842@gmail.com>
2017-06-27 15:31:57 -0700

    commit

--- Classes/Buy Stock IV.java
+++ Classes/Buy Stock IV.java
@@ -1,20 +1,34 @@
+/**
+ * dp[i, j] represents the max profit up until prices[j] using at most i transactions. 
+ * dp[i, j] = max(dp[i, j-1], prices[j] - prices[jj] + dp[i-1, jj]) { jj in range of [0, j-1] }
+ *          = max(dp[i, j-1], prices[j] + max(dp[i-1, jj] - prices[jj]))
+ * dp[0, j] = 0; 0 transactions makes 0 profit
+ * dp[i, 0] = 0; if there is only one price data point you can't make any transaction.
+ */
 public class Solution {
   public int maxProfit(int[] array, int k) {
     // write your solution here
     if (array == null || array.length == 0) {
       return 0;
     }
-    int[] kSum = new int[k * 2];
-    int profit = 0;
-    Arrays.fill(kSum, Integer.MIN_VALUE);
-    for (int i = 0; i < array.length; i++) {
-      int[] prevKSum = Arrays.copyOf(kSum, kSum.length);
-      for (int j = 0, sign = -1; j < Math.min(kSum.length, i + 1); j++, sign *= -1) {
-          int diff = sign * array[i] + (j == 0 ? 0 : prevKSum[j - 1]);
-          kSum[j] = Math.max(diff, prevKSum[j]);
-          profit = Math.max(profit, kSum[j]);
+    if (k >= array.length / 2) {
+      int rst = 0;
+      for (int i = 1; i < array.length; ++i) {
+        if (array[i] > array[i - 1]) {
+          rst += array[i] - array[i - 1];
+        }
+      }
+      return rst;
+    }
+    int[][] dp = new int[k + 1][array.length];
+    for (int i = 1; i <= k; ++i) {
+      int localMax = dp[i - 1][0] - array[0];
+      for (int j = 1; j < array.length; ++j) {
+        dp[i][j] = Math.max(dp[i][j - 1], array[j] + localMax);
+        localMax = Math.max(localMax, dp[i - 1][j] - array[j]);
       }
     }
-    return profit;
+    return dp[k][array.length - 1];
   }
 }
+
