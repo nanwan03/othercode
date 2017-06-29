c04faaa009b90870088a1d01b405d9e156e1683e
nanwan03 <wn1842@gmail.com>
2017-06-28 22:45:56 -0700

    commit

--- Classes/Longest Common Substring.java
+++ Classes/Longest Common Substring.java
@@ -1,11 +1,12 @@
 public class Solution {
   public String longestCommon(String s, String t) {
-    // write your solution here
+    // Write your solution here.
     if (s == null || s.length() == 0 || t == null || t.length() == 0) {
       return "";
     }
     int[][] dp = new int[s.length() + 1][t.length() + 1];
     int longest = 0;
+    int sIndex = -1;
     for (int i = 0; i <= s.length(); i++) {
       for (int j = 0; j <= t.length(); j++) {
         if (i == 0 || j == 0) {
@@ -13,7 +14,10 @@ public class Solution {
         } else {
           if (s.charAt(i - 1) == t.charAt(j - 1)) {
             dp[i][j] = dp[i - 1][j - 1] + 1;
-            longest = Math.max(longest, dp[i][j]);
+            if (dp[i][j] > longest) {
+              longest = dp[i][j];
+              sIndex = i;
+            }
           }
         }
       }
@@ -21,19 +25,7 @@ public class Solution {
     if (longest == 0) {
       return "";
     }
-    StringBuilder sb = new StringBuilder();
-    for (int i = s.length(); i >= 0; i--) {
-      for (int j = t.length(); j >= 0; j--) {
-        if (dp[i][j] == longest) {
-          sb.append(s.charAt(i - 1));
-          longest--;
-          break;
-        }
-      }
-      if (longest == 0) {
-        break;
-      }
-    }
-    return sb.reverse().toString();
+    
+    return s.substring(sIndex - longest, sIndex);
   }
 }
