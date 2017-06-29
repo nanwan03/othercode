6b4737d26a48c07b751fa6f2a63358468f6f86bf
nanwan03 <wn1842@gmail.com>
2017-06-28 17:18:04 -0700

    commit

--- Classes/Max Product Of Cutting Rope.java
+++ Classes/Max Product Of Cutting Rope.java
@@ -1,15 +1,16 @@
 public class Solution {
-  public int maxProduct(int n) {
-    if (n == 0) {
+  public int maxProduct(int length) {
+    // Write your solution here.
+    if (length == 0) {
       return 0;
     }
-    int[] dp = new int[n + 1];
+    int[] dp = new int[length + 1];
     dp[0] = 1;
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= i / 2; j++) {
+    for (int i = 1; i <= length; ++i) {
+      for (int j = 1; j <= i / 2; ++j) {
         dp[i] = Math.max(dp[i], j * Math.max(i - j, dp[i - j]));
       }
     }
-    return dp[n];
+    return dp[length];
   }
 }
