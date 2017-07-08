729dee78450cf00da156f638de3396c820215c5c
nanwan03 <wn1842@gmail.com>
2017-07-08 15:03:35 -0700

    commit

--- Classes/2 Sum Smaller.java
+++ Classes/2 Sum Smaller.java
@@ -1,3 +1,4 @@
+
 public class Solution {
   public int smallerPairs(int[] array, int target) {
     // write your solution here
@@ -5,36 +6,18 @@ public class Solution {
       return 0;
     }
     Arrays.sort(array);
-    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-    int count = 0;
-    for (int i = 0; i < array.length && array[i] < target; i++) {
-      if (target - array[i] <= array[i]) {
-        break;
-      }
-      if (!map.containsKey(array[i])) {
-        map.put(array[i], binarysearch(array, target - array[i]));
-      }
-      count = count + (map.get(array[i]) - i);
-    }
-    return count;
-  }
-  private int binarysearch(int[] array, int target) {
     int left = 0;
     int right = array.length - 1;
-    while (left + 1 < right) {
-      int mid = left + (right - left) / 2;
-      if (array[mid] == target) {
-        right = mid - 1;
-      } else if (array[mid] < target) {
-        left = mid;
+    int rst = 0;
+    while (left < right) {
+      int sum = array[left] + array[right];
+      if (sum >= target) {
+        right--;
       } else {
-        right = mid - 1;
+        rst += right - left;
+        left++;
       }
     }
-    if (array[right] < target) {
-      return right;
-    } else {
-      return left;
-    }
+    return rst;
   }
 }
