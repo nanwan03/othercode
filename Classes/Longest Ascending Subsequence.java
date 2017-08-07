80aef4fc087ece3c0ac841bea183a50af06d8323
nanwan03 <wn1842@gmail.com>
2017-08-06 20:02:45 -0700

    commit

--- Classes/Longest Ascending Subsequence.java
+++ Classes/Longest Ascending Subsequence.java
@@ -3,16 +3,36 @@ public class Solution {
     // Write your solution here.
     int[] dp = new int[array.length];
     int rst = 0;
-    for (int i : array) {
-      int index = Arrays.binarySearch(dp, 0, rst, i);
-      if (index < 0) {
-        index = -(index + 1);
-      }
-      dp[index] = i;
-      if (index == rst) {
-        rst++;
-      }
+    for(int i : array) {
+        int index = search(dp, 0, rst, i);
+        dp[index] = i;
+        if(index == rst) {
+            rst++;
+        }
     }
     return rst;
   }
+  private int search(int[] A, int left, int right, int target) {
+      if (right == 0) {
+          return 0;
+      }
+      right = right - 1;
+      while (left + 1 < right) {
+          int mid = left + (right - left) / 2;
+          if (A[mid] == target) {
+              return mid;
+          } else if (A[mid] < target) {
+              left = mid;
+          } else {
+              right = mid;
+          }
+      }
+      if (A[right] < target) {
+          return right + 1;
+      } else if (A[right] == target || A[left] < target) {
+          return right;
+      } else {
+          return left;
+      }
+    }
 }
