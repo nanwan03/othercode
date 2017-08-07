c669e0fefcf3eb35b01bea073bfa51c93e1366f5
nanwan03 <wn1842@gmail.com>
2017-08-06 19:19:05 -0700

    commit

--- Classes/Form Largest Number.java
+++ Classes/Form Largest Number.java
@@ -1,33 +1,25 @@
 public class Solution {
-  class Cmp implements Comparator<String> {
-    @Override
+  private class Cmp implements Comparator<String> {
     public int compare(String a, String b) {
-      String ab = a.concat(b);
-      String ba = b.concat(a);
-      return ba.compareTo(ab);
+      return (b + a).compareTo(a + b);
     }
   }
   public String largestNumber(String[] input) {
     // Write your solution here.
-    if (input == null || input.length < 1) {
+    if (input == null || input.length == 0) {
       return "";
     }
- 
     Arrays.sort(input, new Cmp());
-    
     StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < input.length; ++i) {
-    	sb.append(input[i]);
+    for (String str : input) {
+      sb.append(str);
     }
-    
-    String rst = sb.toString();
     int i = 0;
-    while (i < input.length && rst.charAt(i) == '0') {
-        i++;
+    for (i = 0; i < sb.length() && sb.charAt(i) == '0'; ++i){}
+    if (i == sb.length()) {
+        return "0";
+    } else {
+        return sb.substring(i);
     }
-    if (i == input.length) {
-      return "0";
-    }
-    return rst.substring(i);
   }
 }
