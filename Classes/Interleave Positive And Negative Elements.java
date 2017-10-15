c5c69dc37c7e2d59789fdf4daf6aa9e1d20fd737
nanwan03 <wn1842@gmail.com>
2017-10-15 16:06:01 -0700

    commit

--- Classes/Interleave Positive And Negative Elements.java
+++ Classes/Interleave Positive And Negative Elements.java
@@ -1,34 +1,28 @@
 public class Solution {
   public int[] interleave(int[] array) {
+    // Write your solution here.
     if (array == null || array.length == 0) {
-        	return array;
-        }
-        int left = 0;
-        int right = array.length - 1;
-        int cur = 0;
-        
-        int posNum = 0;
-        int negNum = 0;
-        while (cur <= right) {
-        	if (array[cur] < 0) {
-        		swap(array, cur++, left++);
-        		posNum++;
-        	} else {
-        		swap(array, cur, right--);
-        		negNum++; 
-        	}
-        }
-        int posIndex = left;
-        int negIndex = 0;
-        while (posIndex < array.length && negIndex < posIndex) {
-        	swap(array, posIndex++, negIndex);
-        	negIndex += 2;
-        }
-        return array;
-  }
-  private void swap(int[] A, int i, int j) {
-    	int temp = A[i];
-    	A[i] = A[j];
-    	A[j] = temp;
+      return array;
     }
+    int next = -1;
+    for (int i = 0; i < array.length; ++i) {
+      if ((i % 2 == 0 && array[i] < 0) || (i % 2 == 1 && array[i] > 0)) {
+        next = Math.max(next, i + 1);
+        for (; next < array.length; ++next) {
+          if (array[next] / array[i] < 0) {
+            break;
+          }
+        }
+        if (next < array.length) {
+          swap(array, i++, next++);
+        }
+      }
+    }
+    return array;
+  }
+  private void swap(int[] array, int i, int j) {
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 }
